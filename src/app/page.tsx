afc7c4234fefa48e76be9d6e7dac93a97ea5839b
Phani2425 <mohantyphanibhusan@gmail.com>
2025-01-04 02:33:24 +0530

    implement dynamic imports for sections to improve server-side rendering performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import { AboutSection } from "@/sections/About";
-import { ContactSection } from "@/sections/Contact";
-import { Footer } from "@/sections/Footer";
-import Grid from "@/sections/Grid";
-import { Header } from "@/sections/Header";
-import { HeroSection } from "@/sections/Hero";
-import { ProjectsSection } from "@/sections/Projects";
-import { TapeSection } from "@/sections/Tape";
-import { TestimonialsSection } from "@/sections/Testimonials";
+import dynamic from 'next/dynamic';
+
+const Header = dynamic(() => import('../sections/Header').then(mod => mod.Header), { ssr: false });
+const HeroSection = dynamic(() => import('../sections/Hero').then(mod => mod.HeroSection), { ssr: false });
+const ProjectsSection = dynamic(() => import('../sections/Projects').then(mod => mod.ProjectsSection), { ssr: false });
+const TapeSection = dynamic(() => import('../sections/Tape').then(mod => mod.TapeSection), { ssr: false });
+const TestimonialsSection = dynamic(() => import('../sections/Testimonials').then(mod => mod.TestimonialsSection), { ssr: false });
+const AboutSection = dynamic(() => import('../sections/About').then(mod => mod.AboutSection), { ssr: false });
+const Grid = dynamic(() => import('../sections/Grid').then(mod => mod.default), { ssr: false });
+const ContactSection = dynamic(() => import('../sections/Contact').then(mod => mod.ContactSection), { ssr: false });
+const Footer = dynamic(() => import('../sections/Footer').then(mod => mod.Footer), { ssr: false });
 
 export default function Home() {
   return (
